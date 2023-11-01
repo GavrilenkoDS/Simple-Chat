f6fe668d4dc9fa582dac9f97a3b557b276555bc2
GavrilenkoDS <72345629+GavrilenkoDS@users.noreply.github.com>
2023-11-02 02:20:42 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,10 +4,9 @@ from threading import Thread
 from datetime import datetime
 
 
-
 SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 5002 # server's port
-separator_token = "<SEP>" # we will use this to separate the client name & message
+SERVER_PORT = 5002 
+separator_token = "<SEP>"
 
 
 s = socket.socket()
@@ -23,24 +22,16 @@ def listen_for_messages():
     while True:
         message = s.recv(1024).decode()
         print("\n" + message)
-
-
 t = Thread(target=listen_for_messages)
-
 t.daemon = True
-
 t.start()
-
 while True:
     to_send =  input()
-   
     if to_send.lower() == 'q':
         break
-   
+        
     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
     to_send = f"[{date_now}] {name}{separator_token}{to_send}"
-    
     s.send(to_send.encode())
-
 
 s.close()
